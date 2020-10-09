a74099b72b5729770630611c5827a6ee7165367c
Redbubble Engineers <engineers@redbubble.com>
2020-10-09 15:56:32 +1100

    Brew formula update for devon version v0.0.2

--- Formula/devon.rb
+++ Formula/devon.rb
@@ -2,16 +2,16 @@
 class Devon < Formula
   desc "Devon, Redbubble's dev environment starter."
   homepage "https://github.com/redbubble/devon"
-  version "0.0.1"
+  version "0.0.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/redbubble/devon/releases/download/v0.0.1/devon_0.0.1_darwin_amd64.tar.gz"
-    sha256 "c50384b5ef690f08c5f3bb221a38b4d5bafd44fe823df7b0b22aae5e626f3775"
+    url "https://github.com/redbubble/devon/releases/download/v0.0.2/devon_0.0.2_darwin_amd64.tar.gz"
+    sha256 "d11eed9da647dcec2602ee8570e7ba803be9fb1227066d67340aa7342b34ab8e"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/redbubble/devon/releases/download/v0.0.1/devon_0.0.1_linux_amd64.tar.gz"
-      sha256 "84c447e92e2e76b66ec3d693d4b1b7423ccea73ebbbd132d486bc854f08290c6"
+      url "https://github.com/redbubble/devon/releases/download/v0.0.2/devon_0.0.2_linux_amd64.tar.gz"
+      sha256 "449bac212d9cc7dedfbb16cda45aaca9aa306559e0b035398e4ba7eae6f6a8e8"
     end
   end
 
